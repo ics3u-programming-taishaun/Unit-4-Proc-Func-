7cac4dd7f0ca5e2a4a5b98318ea07285129db7ef
Taishaun <47784239+TaishaunJ@users.noreply.github.com>
2019-10-31 12:32:14 -0400

    push

---
+++ ConvertToFarenTaishaunJohnson/ConvertToFarenTaishaunJohnson/ConvertToFarenForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ConvertToFarenTaishaunJohnson
+{
+    partial class ConvertToFarenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterTemperature = new System.Windows.Forms.Label();
+            this.txtDegreesC = new System.Windows.Forms.TextBox();
+            this.btnConvertToFar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEnterTemperature
+            // 
+            this.lblEnterTemperature.AutoSize = true;
+            this.lblEnterTemperature.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterTemperature.Location = new System.Drawing.Point(28, 121);
+            this.lblEnterTemperature.Name = "lblEnterTemperature";
+            this.lblEnterTemperature.Size = new System.Drawing.Size(464, 29);
+            this.lblEnterTemperature.TabIndex = 0;
+            this.lblEnterTemperature.Text = "Enter the temperature in degrees Celsius :";
+            // 
+            // txtDegreesC
+            // 
+            this.txtDegreesC.Location = new System.Drawing.Point(571, 130);
+            this.txtDegreesC.Name = "txtDegreesC";
+            this.txtDegreesC.Size = new System.Drawing.Size(100, 20);
+            this.txtDegreesC.TabIndex = 1;
+            // 
+            // btnConvertToFar
+            // 
+            this.btnConvertToFar.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertToFar.Location = new System.Drawing.Point(326, 187);
+            this.btnConvertToFar.Name = "btnConvertToFar";
+            this.btnConvertToFar.Size = new System.Drawing.Size(182, 95);
+            this.btnConvertToFar.TabIndex = 2;
+            this.btnConvertToFar.Text = "Convert to Farenheit";
+            this.btnConvertToFar.UseVisualStyleBackColor = true;
+            this.btnConvertToFar.Click += new System.EventHandler(this.BtnConvertToFar_Click);
+            // 
+            // ConvertToFarenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvertToFar);
+            this.Controls.Add(this.txtDegreesC);
+            this.Controls.Add(this.lblEnterTemperature);
+            this.Name = "ConvertToFarenForm";
+            this.Text = "Convert To Farenheit by Taishaun Johnson";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterTemperature;
+        private System.Windows.Forms.TextBox txtDegreesC;
+        private System.Windows.Forms.Button btnConvertToFar;
+    }
+}
+
---
+++ ConvertToFarenTaishaunJohnson/ConvertToFarenTaishaunJohnson/ConvertToFarenForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertToFarenTaishaunJohnson
+{
+    public partial class ConvertToFarenForm : Form
+    {
+        public ConvertToFarenForm()
+        {
+            InitializeComponent();
+        }
+
+        public void ConvertToFar(double celcius)
+        {
+            // declare the variables
+            double Farenheit;
+
+            // Convert the celcius to farenheit 
+            Farenheit = celcius * (double)9 / (double)5 + 32;
+
+            // Display a message box with celcius converted to Farenheit
+            MessageBox.Show( "The degrees celcius is: " + celcius +  " Which means the Farenheit is: " + Farenheit + " degrees Farenheit ");
+        }
+
+        private void BtnConvertToFar_Click(object sender, EventArgs e)
+        {
+            // declare the variables 
+            double userCelcius;
+
+            // convert the input celcius from a text to a double 
+            userCelcius = Convert.ToDouble(this.txtDegreesC.Text);
+
+            // Call the function to convert from celcis to farenheit
+            this.ConvertToFar(userCelcius);
+        }
+    }
+}
